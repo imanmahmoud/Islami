c8cfc1d4103407c4696770a69c9740698d0abf3e
eman_ <emanmhanafi@gmail.com>
2023-09-21 13:24:40 +0300

    finished radio screen ui.

--- lib/home/radio/radio_tab.dart
+++ lib/home/radio/radio_tab.dart
@@ -3,8 +3,29 @@ import 'package:flutter/material.dart';
 class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/radio_logo.png'),
+        SizedBox(
+          height: 40,
+        ),
+        Text(
+          'إذاعة القرآن الكريم',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset('assets/images/back_icon.png'),
+            Image.asset('assets/images/play_icon.png'),
+            Image.asset('assets/images/next_icon.png'),
+          ],
+        )
+      ],
     );
   }
 }
